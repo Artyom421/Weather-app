3c04f953952807bb82e82f619091953f2e5279a8
Artyom Harutyunyan <artyom.pilot421@gmail.com>
2022-03-15 16:47:13 +0400

    bug fixed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,77 +1,68 @@
 import axios from "axios"
-import { useState , useEffect } from "react"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
 import '../app.css'
+import { changeInputValue, getCityName, getWeatherData } from "../store/actions"
 
 export const Input = () => {
 
-    const [inputValue, setInputValue] = useState('')
-    const [cityName, setCityName] = useState('')
-    const [weatherData, setWeatherData] = useState({})
+    // const initialState = {
 
-    // const changeName = (value) => {
-    //     if(value === '' ) {
-    //         alert('hhhhhhhhhhhh')
-    //     }
-    //     else {
-    //         setCityName(value)
-    //     }
+    //     inputValue:'',
+    //     cityName:'',
+    //     weatherData: ''
     // }
-    // console.log(cityName);
 
-    // const search = async (value) => {
-    //     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=21b17ce1518a5be6b1097495053cb5eb`)
-    //     .then(res => setWeatherData(res.data))
-    //     console.log(weatherData);
-    // }
-    useEffect(
-        async () => {
+    // const [state, setState] = useState(initialState)
+    // const {inputValue, cityName, weatherData} = state
+    // console.log(state);
+   
+    const dispatch = useDispatch()
+    const { inputValue, cityName , weatherData } = useSelector(state => state)
+
+    useEffect( () => {
             
             axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=21b17ce1518a5be6b1097495053cb5eb`)
-            .then(res => {
-                
-                setWeatherData(
-                    {
-                        name: res.data.name,
-                        temperature: res.data.main.temp,
-                        pressure: res.data.main.pressure,
-                        country: res.data.sys.country,
-                        humidity: res.data.main.humidity,
-                        visibility:res.data.visibility,
-                        wind_deg:res.data.wind.deg,
-                        wind_speed:res.data.wind.speed,
-                        icon: ` http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`,
-                        description: res.data.weather[0].description
-                    }
-                )
-                console.log(res.data)
-            })
-            
-
+            .then(res => dispatch(getWeatherData({
+                name: res.data.name,
+                temperature: res.data.main.temp,
+                pressure: res.data.main.pressure,
+                country: res.data.sys.country,
+                humidity: res.data.main.humidity,
+                visibility:res.data.visibility,
+                wind_deg:res.data.wind.deg,
+                wind_speed:res.data.wind.speed,
+                icon: `http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`,
+                description: res.data.weather[0].description
+            })))
     },[cityName])
     
     return (
         <div > 
             <form onSubmit={(e) => {
                 e.preventDefault()
-                setCityName(inputValue)
-                setInputValue('')
+                dispatch(getCityName(inputValue))
+                dispatch(changeInputValue(''))
             }}>
-                <input type="text" value={inputValue} onChange={(e) =>setInputValue(e.target.value)}/>
+                <input placeholder="Enter the city name in here" type="text" value={inputValue} onChange={(e) => dispatch(changeInputValue(e.target.value))
+        }/>
             </form>
-            <div className="data">
-                <span>name: {weatherData.name}</span>
-                <span>temp: {weatherData.temperature} °C</span>
-                <span>pressure: {weatherData.pressure} gPa</span>
-                <span>Country: {weatherData.country}</span>
-                <span>humidity: {weatherData.humidity} %</span>
-                <span>visibility: {weatherData.visibility} m</span>
-                <span>DEG: {weatherData.wind_deg} °</span>
-                <span>speed: {weatherData.wind_speed} m/s</span>
-                <span>description: {weatherData.description}</span>
-                <span>icon: <img src={weatherData.icon} /></span>
-                
-            </div>
+            {
+                weatherData === '' ? null :
+                <div className="data">
+                    <span>name: {weatherData.name}</span>
+                    <span>temp: {Math.floor(weatherData.temperature)} °C</span>
+                    <span>pressure: {Math.floor(weatherData.pressure * 0.750062)} mmHg</span>
+                    <span>Country: {weatherData.country}</span>
+                    <span>humidity: {weatherData.humidity} %</span>
+                    <span>visibility: {weatherData.visibility} m</span>
+                    <span>DEG: {weatherData.wind_deg} °</span>
+                    <span>speed: {weatherData.wind_speed} m/s</span>
+                    <span>description: {weatherData.description}</span>
+                    <span>icon: <img src={weatherData.icon} alt='img'/></span>
+                </div>
+            }
         </div>
     )
 }
--- src/store/reducer/reducer.js
+++ src/store/reducer/reducer.js
@@ -3,7 +3,7 @@ import { CHANGE_INPUT_VALUE, GET_CITY_NAME, GET_WEATHER_DATA } from "../actions"
 const initialState = {
     inputValue: '',
     cityName: '',
-    weatherData: {}
+    weatherData: ''
 }
 
 
@@ -14,9 +14,9 @@ export const Reducer = ( state = initialState, action ) => {
         case CHANGE_INPUT_VALUE:
             return{...state, inputValue:action.payload}
         case GET_CITY_NAME:
-            return{...state, inputValue:action.payload}
+            return{...state, cityName:action.payload}
         case GET_WEATHER_DATA:
-            return{...state, inputValue:action.payload}
+            return{...state, weatherData:action.payload}
         default: 
             return state;
     } 
